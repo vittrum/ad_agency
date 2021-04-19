aba61b2c2a2a46c59f04f189f854eccd48566e0e
Viktor <viktorkovalskyi9@gmail.com>
2021-04-19 23:10:58 +0300

    Initial commit

---
+++ ads/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+# Create your models here.
+from users.models import Client, Employee
+
+
+class Contract(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    begin_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField(default=None, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.client.last_name} by {self.employee.last_name}'
+
+    class Meta:
+        db_table = 'contracts'
+
+
+class Bill(models.Model):
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+    print_date = models.DateTimeField(auto_now_add=True)
+    sum = models.IntegerField()
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.is_paid:
+            return 'Bill paid'
+        else:
+            return 'Not paid'
+
+
+class BaseAd(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField(default=1)
+    efficiency = models.IntegerField(default=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+# TODO: add relative name
+class OutdoorAd(BaseAd):
+    height = models.IntegerField(default=1)
+    width = models.IntegerField(default=1)
+    type = models.CharField()
+    contract = models.ManyToManyField(Contract)
+
+    class Meta:
+        db_table = 'outdoor_ads'
+
+
+# TODO: add relative name
+class TVAd(BaseAd):
+    audience_coeff = models.IntegerField(default=100)
+    duration = models.IntegerField(default=30)
+    contract = models.ManyToManyField(Contract)
+
+    class Meta:
+        db_table = 'tv_ads'
+
+
+# TODO: add relative name
+class InternetAd(models.Model):
+    type = models.CharField(max_length=100, default='context_ad')
+    contract = models.ManyToManyField(Contract)
+    audience_coeff = models.IntegerField(default=100)
+    specs = models.CharField(max_length=100, default='no specs')
+
+    class Meta:
+        db_table = 'internet_ads'
---
+++ users/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
+
+    class Meta:
+        db_table = 'clients'
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
+
+    class Meta:
+        db_table = 'employees'
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'departments'
+
+
+class EmployeeDepartment(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100, default='employee')
+
+    def __str__(self):
+        return f'{self.employee.last_name} from {self.department.name}'
+
+    class Meta:
+        db_table = 'employees_departments'
